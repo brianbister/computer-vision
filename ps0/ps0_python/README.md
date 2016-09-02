8abb6cc4e63dc155373d9b29c06d889f638ad6b5
Brian Bister <brianbister@gmail.com>
2016-09-01 18:43:42 -0700

    Add replace pixels function.

--- ps0/ps0_python/README.md
+++ ps0/ps0_python/README.md
@@ -2,4 +2,4 @@
 
 `Which looks more like what you’d expect a monochrome image to look like? Would you expect a computer vision algorithm to work on one better than the other?`
 
-It's difficult to say but the red channel looks more monochrome than the green. With the image I chose the red channel has more distinct edges, the green feels like there are several more shades of green in the picture which is why it feels like red may be better suited for computer vision algorithms.
+It's difficult to say but the red channel looks more monochrome than the green. With the image I chose the red channel has more distinct edges, the green feels like there are several more shades of green in the picture which is why it feels like red may be better suited for computer vision algorithms. However, I feel these results may vary based on the contents of your image.
--- ps0/ps0_python/ps0.py
+++ ps0/ps0_python/ps0.py
@@ -19,6 +19,18 @@ def basic_input(images):
         cv2.imwrite("output/ps0-1-a-{}.png".format(i + 1), img)
 
 
+def _create_monochrome_image(image, color):
+    """Given an image and color, create the monochrome version of it.
+
+    image: The location of the image.
+    color: The color to create a monochrome version of.
+    """
+    mono_img = np.copy(image)
+    mono_img[:, :, (color + 1) % 3] = 0
+    mono_img[:, :, (color + 2) % 3] = 0
+    return mono_img
+
+
 def color_planes(image):
     """Given an image, create 3 new images with the following:
     1) The red and blue pixels swapped.
@@ -37,17 +49,38 @@ def color_planes(image):
     cv2.imwrite("output/ps0-2-a-1.png", swap_img)
 
     # Green monochrome.
-    green_img = np.copy(img)
-    green_img[:, :, BLUE] = 0
-    green_img[:, :, RED] = 0
+    green_img = _create_monochrome_image(img, GREEN)
     cv2.imwrite("output/ps0-2-b-1.png", green_img)
 
     # Red monochrome.
-    red_img = np.copy(img)
-    red_img[:, :, BLUE] = 0
-    red_img[:, :, GREEN] = 0
+    red_img = _create_monochrome_image(img, RED)
     cv2.imwrite("output/ps0-2-c-1.png", red_img)
 
+
+def replace_pixels(image_1, image_2, color):
+    """Creates a monochrome version of both images then places the 100x100
+    center of image_1 into the center of image_2
+
+    image_1: The location of image_1
+    image_2: The locatino of image_2
+    color: The color to create the monochrome version.
+    """
+    img_1 = cv2.imread(image_1)
+    img_2 = cv2.imread(image_2)
+    mono_1 = _create_monochrome_image(img_1, color)
+    mono_2 = _create_monochrome_image(img_2, color)
+
+    # Get center of green_1
+    height_1, width_1, _ = mono_1.shape
+    print height_1, width_1
+    center_1 = mono_1[height_1 / 2 - 50:height_1 / 2 + 50,
+                      width_1 / 2 - 50:width_1 / 2 + 50]
+
+    # Replace center of green_2
+    height_2, width_2, _ = mono_2.shape
+    mono_2[height_2 / 2 - 50:height_2 / 2 + 50,
+           width_2 / 2 - 50:width_2 / 2 + 50] = center_1
+    cv2.imwrite("output/ps0-3-a-1.png", mono_2)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run some basic computer "
@@ -57,9 +90,13 @@ if __name__ == "__main__":
     parser.add_argument("-img", "--images", action="store", dest="images",
                         help="The list of images to process.", nargs="+")
     args = parser.parse_args()
-    if args.question_num == 0:
-        basic_input(args.images)
     if args.question_num == 1:
+        basic_input(args.images)
+    if args.question_num == 2:
         if len(args.images) != 1:
             raise ValueError("Requires only one image.")
         color_planes(args.images[0])
+    if args.question_num == 3:
+        if len(args.images) != 2:
+            raise ValueError("Requires two images.")
+        replace_pixels(args.images[0], args.images[1], GREEN)
